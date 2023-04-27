d1ab16f5389285b40bee88118b72d280cbb5975e
Tejendra Pal Singh <110801292+tejth@users.noreply.github.com>
2023-04-27 17:01:12 +0530

    Add files via upload

---
+++ Basic Programs/addition.java
@@ -0,0 +1,14 @@
+//TO PRINT ADDITION OF TWO NUMBERS.
+import java.util.*;
+
+public class addition{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int a = sc.nextInt();
+        System.out.println("enter other number");
+        int b = sc.nextInt();
+         System.out.println("THE SUM OF NUMBERS IS : "+" "+ (a+b));
+        
+    }
+}
